be3cdba63b3eca2b7d9b8d9ab40b664edbdbcca0
AresSkeleton <54546114+AresSkeleton@users.noreply.github.com>
2020-04-07 09:57:28 +0200

    07.04 backend

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ const path = require("path");
 const db = require('./config/database');
 const app = express();
 const port = 1337;
+
+
 const routerIndex = require('./routes/index');
+const routerLogin = require('./routes/login');
+
+
+
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: false}));
 app.use(express.static(__dirname + '/public'));
 
 
@@ -22,6 +30,8 @@ db.authenticate().then(() =>{
 app.get('/', routerIndex);
 // app.render
 app.get('/login', routerIndex);
+app.get('/register', routerLogin);
+app.post('/register', routerLogin);
 // app.get('/user', router);
 
 app.listen(port, console.log(`Serwer urochomiony na porcie 1337`));
--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 
+router.get('/register', function(req, res) {
+    res.render('register',{ });
+});
 
+router.post('/register', async function(req, res){
+    try{
+        const hashedPass = await bcrypt.hash(req.body.passwordInput);
+        
+    }catch{
 
+    }
+    console.log(req.body.passwordInput);
+});
 
 
 module.exports = router;
